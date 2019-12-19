13b2a6cc64bacf258dc9a248dc8f455f3bf01121
oleksandrNAU <on4@gcloud.ua.es>
2019-12-19 19:11:17 +0100

    ff

--- C6-04-12/ejemplos/ejercicio1.js
+++ C6-04-12/ejemplos/ejercicio1.js
@@ -4,10 +4,9 @@ function pulsa(){
         let name = prompt("Pon mensaje premo", "nom");
         let text= document.createTextNode("Tu nombre es "+name);
         let p=document.getElementsByTagName("p")[0];
-        if(p.innerText==null){
+        if(p.innerText==false){
         p.appendChild(text);}
         else{
-            p.innerText=null;
-            p.appendChild(text);
+          return;
         }
 }
