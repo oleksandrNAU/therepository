1921af9ca3979b4faf5f33eb1625e01d0fbd8040
oleksandrNAU <on4@gcloud.ua.es>
2020-01-17 17:48:31 +0100

    ff

--- Angular/angular-products/src/app/product-list/product-list.component.ts
+++ Angular/angular-products/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../interfaces/i-product';
 
 @Component({
   selector: 'product-list',
@@ -8,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
   title = 'Mi lista de productos';
   headers = {description: 'Producto', price: 'Precio', available: 'Disponible'};
+  products: IProduct[] = [{
+    id: 1,
+    description: 'SSD hard drive',
+    available: new Date('2016-10-03'),
+    price: 75,
+    imageUrl: 'assets/ssd.jpg',
+    rating: 5
+  }, {
+    id: 2,
+    description: 'LGA1151 Motherboard',
+    available: new Date('2016-09-15'),
+    price: 96.95,
+    imageUrl: 'assets/motherboard.jpg',
+    rating: 4
+  }];
   constructor() { }
 
   ngOnInit() {
